6d07a291eb2d30d2d9a336b18f6ebcbb5add3c41
Jason <jasonbetsargon@gmail.com>
2024-09-01 09:55:38 -0700

    Prompt user to change grid size. Generate new grid accordingly

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const container = document.querySelector('.container');
 container.style.cssText = "display: flex";
 
+const button = document.querySelector('button');
+
 for (let i = 1; i <= 256; i++) {
     let item = document.createElement('div');
     item.classList.add("gridItem");
@@ -13,6 +15,27 @@ function randomRGB() {
     return Math.floor((Math.random() * 256));
 }
 
+// Grid generator
+function gridGenerator(size) {
+    // Delete grid
+    while(container.firstChild){
+        container.removeChild(container.firstChild);
+    }
+
+    // Set container height and width according to grid size
+    let gridLength = size*40;
+    container.style.height = `${gridLength}px`;
+    container.style.width = `${gridLength}px`;
+    for (let i = 1; i <= size*size; i++) {
+        let item = document.createElement('div');
+        item.classList.add("gridItem");
+        item.dataset.cell = i;
+        container.appendChild(item);
+    }
+}
+// Generate first grid automatically
+gridGenerator(16);
+
 // Identify each cell when hovering over with mouse
 container.addEventListener('mouseover', (e) => {
     console.log('hovered ' + e.target.dataset.cell);
@@ -23,3 +46,7 @@ container.addEventListener('mouseover', (e) => {
     e.target.style.backgroundColor = `rgb(${red},${green},${blue})`;
 });
 
+button.addEventListener('click', (e) => {
+    let gridSize = prompt("Enter grid size:", 16);
+    gridGenerator(gridSize);
+});
