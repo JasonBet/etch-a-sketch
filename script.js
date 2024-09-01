fe2d346f6151d71fe731c74d60ad676cc74398dc
Jason <jasonbetsargon@gmail.com>
2024-08-31 18:31:22 -0700

    Successfully identify each cell on mouseover

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const container = document.querySelector('.container');
-container.cssText = "display: flex";
+container.style.cssText = "display: flex";
 
 for (let i = 1; i <= 256; i++) {
     let item = document.createElement('div');
     item.classList.add("gridItem");
+    item.id = i;
     container.appendChild(item);
 }
+
+container.addEventListener('mouseover', (e) => {
+    console.log('clicked ' + e.target.id);
+});
