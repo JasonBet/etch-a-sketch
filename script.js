940ee4e1bb4780d88f7234afc9d8e23e81766407
Jason <jasonbetsargon@gmail.com>
2024-09-01 09:19:45 -0700

    Randomize cell color when hovering over

--- script.js
+++ script.js
@@ -8,6 +8,18 @@ for (let i = 1; i <= 256; i++) {
     container.appendChild(item);
 }
 
+// Randomizer for RGB value (return random number between 0-255)
+function randomRGB() {
+    return Math.floor((Math.random() * 256));
+}
+
+// Identify each cell when hovering over with mouse
 container.addEventListener('mouseover', (e) => {
-    console.log('clicked ' + e.target.dataset.cell);
+    console.log('hovered ' + e.target.dataset.cell);
+    let red = randomRGB();
+    let green = randomRGB();
+    let blue = randomRGB();
+    console.log("Red is: " + red + " Green is: " + green + " Blue is: " + blue);
+    e.target.style.backgroundColor = `rgb(${red},${green},${blue})`;
 });
+
